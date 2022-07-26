e7bbfb22e08062c24c21fb8367889367ea3f4da9
ChiaraMameli <mameli.chiara@libero.it>
2022-07-26 16:55:06 +0200

    added header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,16 +1,32 @@
 <template>
 <header>
+    <img :src="logo" alt="">
 </header>
 </template>
 
 <script>
 export default {
     name: 'BaseHeader',
+    data(){
+        return{
+            logo: require('../assets/img/logo.svg')
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../assets/sass/_vars.scss';
+
+header {
+    height: 60px;
+    background-color: $light_blue;
+    padding: 10px;
+
     img{
-        color: black
-    }
+        max-height: 100%;
+        filter: invert(71%) sepia(54%) saturate(475%) hue-rotate(67deg) brightness(94%) contrast(91%);
+        margin-left: 10px;
+        }
+}
 </style>
