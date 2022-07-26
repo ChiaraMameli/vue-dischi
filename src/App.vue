cc5ac34aeaef5cc45d0e815c5fe497928b923a7a
ChiaraMameli <mameli.chiara@libero.it>
2022-07-26 15:33:26 +0200

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <BaseHeader />
+  <ContentSection />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseHeader from './components/BaseHeader.vue'
+import ContentSection from './components/ContentSection.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseHeader,
+    ContentSection
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/sass/style.scss';
 </style>
